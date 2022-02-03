e4861014d9526e17109a7a780c039f0d40e67832
Tomasz Cekało <tomasz.cekalo@gmail.com>
2022-02-03 21:37:06 +0100

    Add project files.

---
+++ PalmerPenguins/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Data downloaded from https://github.com/allisonhorst/palmerpenguins/tree/master/inst/extdata
+
+Console.WriteLine("Hello, World!");
+
+////Load sample data
+//var sampleData = new PalmerPenguins.ModelInput()
+//{
+//    Island = @"Torgersen",
+//    Bill_length_mm = 39.5F,
+//    Bill_depth_mm = 17.4F,
+//    Flipper_length_mm = 186F,
+//    Body_mass_g = 3800F,
+//    Sex = @"female",
+//    Year = 2007F,
+//};
+
+////Load model and predict output
+//var result = PalmerPenguins.Predict(sampleData);
